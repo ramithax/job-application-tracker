641bb7ac66c97370140fae26a282665f497db389
ramitha <ramithamanilka2003@gmail.com>
2026-06-24 23:59:49 +0530

    sign up page

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,8 +2,8 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.NEXT_PUBLIC_BETTER_AUTH_URL!);
-
+const client = new MongoClient(process.env.MONGODB_URI!);
+await client.connect();
 const db = client.db();
 
 export const auth = betterAuth({
